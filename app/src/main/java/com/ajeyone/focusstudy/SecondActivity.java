25133296f2c19e06589c8f9041c5e73d6d360cf1
Wang Junjie <ajeyone@163.com>
2021-03-17 09:36:53 +0800

    全局焦点事件

--- app/src/main/java/com/ajeyone/focusstudy/SecondActivity.java
+++ app/src/main/java/com/ajeyone/focusstudy/SecondActivity.java
@@ -1,14 +1,26 @@
 package com.ajeyone.focusstudy;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        View view = findViewById(R.id.title);
+        view.getViewTreeObserver().addOnGlobalFocusChangeListener(new ViewTreeObserver.OnGlobalFocusChangeListener() {
+            @Override
+            public void onGlobalFocusChanged(View oldFocus, View newFocus) {
+                Log.d(TAG, "onGlobalFocusChanged: oldFocus=" + oldFocus);
+                Log.d(TAG, "onGlobalFocusChanged: newFocus=" + newFocus, new Exception());
+            }
+        });
     }
 }
