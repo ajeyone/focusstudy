ceae6e026ae19e267ff97ea6cff898a0d4acccd2
Wang Junjie <ajeyone@163.com>
2021-03-19 00:39:02 +0800

    复杂的自定义样式，例1：drawable 定义多种状态

--- app/src/main/java/com/ajeyone/focusstudy/SecondActivity.java
+++ app/src/main/java/com/ajeyone/focusstudy/SecondActivity.java
@@ -3,11 +3,12 @@ package com.ajeyone.focusstudy;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "SecondActivity";
 
     @Override
@@ -20,6 +21,28 @@ public class SecondActivity extends AppCompatActivity {
             public void onGlobalFocusChanged(View oldFocus, View newFocus) {
                 Log.d(TAG, "onGlobalFocusChanged: oldFocus=" + oldFocus);
                 Log.d(TAG, "onGlobalFocusChanged: newFocus=" + newFocus, new Exception());
+            }
+        });
+
+        setupButtonContainer(findViewById(R.id.button_container), findViewById(R.id.button_resume));
+        setupButtonContainer(findViewById(R.id.button_container2), findViewById(R.id.button_resume2));
+    }
+
+    @Override
+    public void onClick(View v) {
+        v.setSelected(!v.isSelected());
+    }
+
+    private void setupButtonContainer(final ViewGroup container, View resumeButton) {
+        final int n = container.getChildCount();
+        for (int i = 0; i < n; i++) {
+            container.getChildAt(i).setOnClickListener(this);
+        }
+
+        resumeButton.setOnClickListener(v -> {
+            final int n1 = container.getChildCount();
+            for (int i = 0; i < n1; i++) {
+                container.getChildAt(i).setSelected(false);
             }
         });
     }
